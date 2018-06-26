42e5c6dc5008ba930496b3855d4340a0dff90c82
Miguel <martinmiguel798@gmail.com>
2018-06-26 08:24:23 -0500

    Controladores subidos

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 
-require_once "controllers/template.controller.php";
+require_once "controllers/TemplateController.php";
+
 	$template = new TemplateController();
 	$template -> ctrTemplate(); 
